d56058523bb4553d468f04c12bee9a3bf6aeb726
Yukimi <yukimi@arksystems.co.jp>
2019-03-06 13:06:53 +0900

    FistCommit

---
+++ src/main/java/com/cisco/japan/as/uran/payloadencoder/PayloadEncoder.java
@@ -0,0 +1,9 @@
+package com.cisco.japan.as.uran.payloadencoder;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public interface PayloadEncoder {
+
+	public JsonNode encode( String[] payloadStrings) throws Exception;
+
+}
