49e8340fd02abe0ba664527d62c5929150bc46f2
Yukimi <yukimi@arksystems.co.jp>
2019-03-15 15:37:55 +0900

    In/Out 用のI/Fの変更

--- src/main/java/com/cisco/japan/as/uran/payloadencoder/PayloadEncoder.java
+++ src/main/java/com/cisco/japan/as/uran/payloadencoder/PayloadEncoder.java
@@ -1,9 +1,35 @@
 package com.cisco.japan.as.uran.payloadencoder;
 
+import java.util.List;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
+
+/**
+ * 返却値は下記のJsonNodeのList。
+ *  １つのJsonNode は下記を表現する
+	"decoded_payload": 
+		{
+			"uplink_time": "2014-10-10T13:50:40+09:00", // ISO 8601準拠
+			"encoded_payload": "0c1002789271999371a10",
+			"payload_json": {
+				"protocol": "0c1002",
+				"Longitude": "139.75495999999998",
+				"Latitude": "35.649792999999995",
+				"GpsFixStatus": "not fix",
+				"ReportType": "Periodic mode repport",
+				"BatteryCapacity": "65",
+				"Date&Time": "2014-10-10T13:50:40+09:00" // ISO 8601準拠
+			}
+		}
+ * 
+ * 
+ * 
+ * @author ykurozum
+ *
+ */
 public interface PayloadEncoder {
 
-	public JsonNode encode(String[] payloadStrings) throws Exception;
+	public List<JsonNode> encode(List<EncodedPayload> encodedPayloadList) throws Exception;
 
 }
