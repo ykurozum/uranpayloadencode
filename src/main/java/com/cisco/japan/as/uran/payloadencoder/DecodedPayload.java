48dfdbc9210e6518835dd8ee3c3d8e6be8b4db0e
Yukimi <yukimi@arksystems.co.jp>
2019-03-18 15:18:26 +0900

    In/Outのクラスを追加修正

---
+++ src/main/java/com/cisco/japan/as/uran/payloadencoder/DecodedPayload.java
@@ -0,0 +1,54 @@
+package com.cisco.japan.as.uran.payloadencoder;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class DecodedPayload {
+	/**
+	 *  データ自体が表す特定の時間（通常はデータの採取時刻）
+	 */
+	Date  time;
+	/**
+	 * デコード前のpayload(通常は対応するEncodedPayloadの値をセット）
+	 */
+	String payloadString;
+	/**
+	 * payloadデコード後のJSON（デバイス種類によって様々）
+	 */
+	JsonNode palyloadJson;
+	/**
+	 * デバイスの識別子（デコードによってEncodedPayloadから変更の可能性もある）
+	 */
+	String deviceIdentifiyer;
+	
+	public DecodedPayload(Date time, String payloadString) {
+		this.time = time;
+		this.payloadString = payloadString;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	public String getPayloadString() {
+		return payloadString;
+	}
+	public void setPayloadString(String payloadString) {
+		this.payloadString = payloadString;
+	}
+	public JsonNode getPalyloadJson() {
+		return palyloadJson;
+	}
+	public void setPalyloadJson(JsonNode palyload_json) {
+		this.palyloadJson = palyload_json;
+	}
+	public String getDeviceIdentifiyer() {
+		return deviceIdentifiyer;
+	}
+	public void setDeviceIdentifiyer(String deviceIdentifiyer) {
+		this.deviceIdentifiyer = deviceIdentifiyer;
+	}
+
+}
--- src/main/java/com/cisco/japan/as/uran/payloadencoder/EncodedPayload.java
+++ src/main/java/com/cisco/japan/as/uran/payloadencoder/EncodedPayload.java
@@ -3,8 +3,18 @@ package com.cisco.japan.as.uran.payloadencoder;
 import java.util.Date;
 
 public class EncodedPayload {
+	/**
+	 * payloadデータがアップロードされた時間
+	 */
 	Date  time;
+	/**
+	 * デコード対象のpayloadのデータ文字列
+	 */
 	String payloadString;
+	/**
+	 * payloadStringを運搬したデバイスの識別子
+	 */
+	String deviceIdentifiyer;
 	
 	public EncodedPayload(Date time, String payloadString) {
 		this.time = time;
